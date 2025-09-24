24ee1b246c6d46ad498b6c70c3ce38f47f79decc
Dhamu <ramanofficials18@gmail.com>
2025-09-24 23:09:55 +0530

    build error fix

--- src/components/industry/our-clients/index.tsx
+++ src/components/industry/our-clients/index.tsx
@@ -7,8 +7,6 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
-// import required modules
-import { Pagination } from "swiper/modules";
 import HeadingSectionDetail from "@/components/common/heading";
 import Image from "next/image";
 
