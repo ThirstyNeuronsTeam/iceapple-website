9d0ac6e4d901e8362739089f06a4b362c7f1ab0f
veerendra.singavarapu <veerendra.singavarapu@iceapple.tech>
2025-10-09 17:10:32 +0530

    Issue fixes

--- src/components/common/banner-section-detail/index.tsx
+++ src/components/common/banner-section-detail/index.tsx
@@ -21,7 +21,7 @@ const BannerSectionDetail: React.FC<HeroSectionProps> = ({
   isBtnShow,
 }) => {
   const handleScroll = () => {
-    const hero = document.getElementById("hero"); // Hero should always have an id
+    const hero = document.getElementById("hero");
     const afterHero = hero?.nextElementSibling as HTMLElement;
     afterHero?.scrollIntoView({ behavior: "smooth" });
   };
@@ -29,14 +29,15 @@ const BannerSectionDetail: React.FC<HeroSectionProps> = ({
     <div
       className={clsx(
         mainClassName,
-        "relative z-10 h-full flex order-1 sm:order-2"
+        // Responsive flex direction tweaks for smaller screens
+        "relative z-10 h-full sm:pt-16 flex flex-col sm:flex-row order-1 sm:order-2"
       )}
     >
-      <div className="w-full max-w-[280px] sm:max-w-none space-y-6 pr-5 sm:pr-0 ms-auto">
+      <div className="w-full max-w-[280px] sm:max-w-none space-y-6 pr-4 sm:pr-16 sm:pr-0 ms-auto">
         <div className="overflow-hidden">
           <div className="space-y-3 sm:space-y-6 lg:pl-10">
             <div className="text-[#0B68FF]">
-              <h1 className="flex flex-col  font-extrabold leading-none text-[clamp(1.75rem,4vw,4.5rem)]">
+              <h1 className="flex flex-col font-extrabold leading-none text-[clamp(1.75rem,4vw,4.5rem)] break-words max-w-full">
                 {title?.map((item, index) => (
                   <div className="w-max" key={index}>
                     {item}
@@ -56,13 +57,14 @@ const BannerSectionDetail: React.FC<HeroSectionProps> = ({
               </Link>
             )}
           </div>
-          <span
-            onClick={handleScroll}
-            className="cursor-pointer absolute right-10 sm:top-3/4 xl:top-1/4 transform -rotate-90 text-xl tracking-wide hidden lg:block font-bold"
-          >
-            Scroll Down
-          </span>
         </div>
+        {/* Hide "Scroll Down" button on screens smaller than xl */}
+        <span
+          onClick={handleScroll}
+          className="cursor-pointer absolute right-4 xl:right-10 top-auto bottom-6 xl:bottom-auto xl:top-1/4 transform -rotate-90 text-xl tracking-wide font-bold hidden xl:block"
+        >
+          Scroll Down
+        </span>
       </div>
     </div>
   );
