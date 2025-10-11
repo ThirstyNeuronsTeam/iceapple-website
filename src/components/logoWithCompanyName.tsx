42b889a3d1ddd98ac7fceffa38a80e3bfd945c62
Dhamu <ramanofficials18@gmail.com>
2025-10-11 23:23:10 +0530

    logo align in footer and menu

--- src/components/logoWithCompanyName.tsx
+++ src/components/logoWithCompanyName.tsx
@@ -6,7 +6,7 @@ const LogoWithCompanyName: React.FC = () => {
   return (
     <Link
       href="/"
-      className="grid grid-cols-[25px_90px] md:grid-cols-[60px_200px] gap-2 md:gap-5 items-baseline"
+      className="grid grid-cols-[25px_90px] md:grid-cols-[40px_160px] lg:grid-cols-[60px_200px] gap-2 md:gap-5 items-baseline"
     >
       <Logo />
       <CompanyName />
